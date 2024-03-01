81ad286bbc873d1e442d21aa2573f013f4bd231c
reymondv <reymondyv@gmail.com>
2024-03-01 16:40:12 +0800

    updated portfolio details and resume

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,22 @@ import React from 'react';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className='flex justify-center text-sm my-3 opacity-60 font-300 font-mono px-5 text-center md:px-0'>
-      &copy; {year} Reymond Villanueva. All Rights Reserved.
+    <footer className='flex flex-col justify-center text-sm my-3 opacity-80 font-300 font-mono px-5 text-center md:px-0'>
+      <div>
+        Built with{' '}
+        <span className='hover:text-cyan-400 text-orange-400'>
+          ReactJS TypeScript
+        </span>
+        ,{' '}
+        <span className='hover:text-cyan-400 text-orange-400'>TailwindCSS</span>
+        , and{' '}
+        <span className='hover:text-cyan-400 text-orange-400'>
+          Framer Motion
+        </span>
+        . Deployed throgh{' '}
+        <span className='hover:text-cyan-400 text-orange-400'>Vercel</span>
+      </div>
+      <div>&copy; {year} Reymond Villanueva. All Rights Reserved.</div>
     </footer>
   );
 };
--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -4,6 +4,7 @@ import { AnimatedName } from '../util/animation';
 import { useAnimation, useInView } from 'framer-motion';
 import { motion } from 'framer-motion';
 import { stackContainer, container, item } from '../util/variants';
+import { FaDownload } from 'react-icons/fa';
 import Card from '../util/Card';
 
 const About = () => {
@@ -17,7 +18,7 @@ const About = () => {
   const stackRef = useRef(null);
   const isInView = useInView(ref, { once: true });
   const isInViewStack = useInView(stackRef, { once: true });
-  console.log(data);
+
   useEffect(() => {
     if (isInView) {
       controls.start('visible');
@@ -36,7 +37,8 @@ const About = () => {
     <div className='m-auto'>
       <div
         className='flex flex-col items-center justify-center md:px-4 md:flex-row mb-3 md:mb-12'
-        ref={ref}>
+        ref={ref}
+      >
         <div className='mr-0 md:mr-4 md:mb-0 mb-4'>
           <img
             className='m-auto rounded-full aspect-square'
@@ -50,23 +52,33 @@ const About = () => {
           initial='hidden'
           animate={controls}
           variants={container}
-          className='border-t-2 pt-2 border-white border-opacity-60 md:pl-4 md:border-l-2 md:border-t-0 md:pt-0'>
+          className='border-t-2 pt-2 border-white border-opacity-60 md:pl-4 md:border-l-2 md:border-t-0 md:pt-0'
+        >
           <AnimatedName isInView={isInView} />
           {description.map((description, index) => (
             <motion.p
               key={index}
               className='opacity-50 text-lg md:text-base text-comment tracking-normal my-2 md:mb-0'
-              variants={item}>
+              variants={item}
+            >
               // {description}
             </motion.p>
           ))}
+          <a
+            className='cursor-pointer flex flex-row gap-2 hover:text-cyan-400 mt-2'
+            href={data.cv.file}
+            target='_blank'
+          >
+            <FaDownload className='my-auto' /> Download CV
+          </a>
         </motion.div>
       </div>
       <motion.div
         ref={stackRef}
         initial='hidden'
         animate={controls}
-        variants={stackContainer}>
+        variants={stackContainer}
+      >
         <h1 className='mb-3 text-lg'>
           <i className='fas fa-layer-group'></i>&nbsp;Technology stack
         </h1>
